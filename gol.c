8685d1904c165217f2f15709bbe78da6866e5263
franck <alphacentorus@gmail.com>
2012-12-27 16:27:25 +0100

    simplification et supression de fonctions.c et header.h

--- gol.c
+++ gol.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define X 15
 #define Y 40
@@ -8,7 +9,13 @@
 /************** Variables globales ****************
 *                                                  *
 ***************************************************/
+
 int NbrCell = 0;
+
+int rdtsc(){
+	__asm__ __volatile__("rdtsc");
+}
+
 
 
 void Origin_Cells(int Size_Row,  int Size_col, int tab_cells[Size_Row][Size_col]  );
@@ -20,9 +27,10 @@ void Origin_Cells(int Size_Row,  int Size_col, int tab_cells[Size_Row][Size_col]
 
 int main(void)
 {
+	
 	int Origin_tab[X][Y];
 	Origin_Cells(X,Y,Origin_tab);
-
+	
 	printf("%d \n", NbrCell  );
 	
 	
@@ -39,7 +47,7 @@ void Origin_Cells(int Size_Row,  int Size_col, int tab_cells[Size_Row][Size_col]
 			if( i == (Size_Row -1) ||i == 0 || j == 0 || j == (Size_col -1)) { /* on délimite les bordures du tableau */
 				tab_cells[i][j] = -1;
 			} else {
-
+				srand(rdtsc()); /*fonction srand basé sur la fréquence du processeur*/
 				tab_cells[i][j]= rand()% 2; /* utilisation modulo partie restante 0 ou 1 */
 
 				if (tab_cells[i][j] == 1){ /* Si valeur 1 on augmente la population de 1 */
--- header.h
+++
@@ -1,4 +0,0 @@
-
-
-
-int Origin_Cells(int Size_Row,  int Size_col, int tab_cells[Size_Row][Size_col]  );
